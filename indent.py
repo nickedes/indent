25e2b679b8c5222f985c53a9e6d25147f24c4948
nikhil <nkmittal4994@gmail.com>
2015-07-26 18:37:09 +0530

    Type check for count and raises TypeError if of not correct type as specified.

--- indent.py
+++ indent.py
@@ -1,11 +1,23 @@
-def indentation(string, indent):
-    """Indent a string.
+def indentation(string, indent, count):
+    """Indents a string.
 
     Keyword arguments:
     string -- The string you want to indent.
     indent -- The string to use for the indent.
+    count -- How many times you want indent repeated.
     """
-    if type(string) is str and type(indent) is str:
-        pass
-    else:
+    # Type check for string and indent
+    if type(string) is not str or type(indent) is not str:
         raise TypeError("Should be string")
+    else:
+        pass
+    # Type check for count
+    if type(count) is not int:
+        raise TypeError("count should be Integer")
+    else:
+        pass
+    if count == 0:
+        return string
+
+
+print(indentation('Uni\nRain', 'd', 0))
