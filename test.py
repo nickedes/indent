add032eab5b40bfc1f55516d99a5fcf40a1c1563
nikhil <nkmittal4994@gmail.com>
2015-07-27 20:48:03 +0530

    A test which checks for actual indentation.

--- test.py
+++ test.py
@@ -4,3 +4,6 @@ from indent import indentation
 class TestClass:
     def test_count(self):
         assert indentation('str', '$', 0) == 'str'
+
+    def test_indent(self):
+        assert indentation('str\nhi', '$', 1) == '$str\n$hi'
