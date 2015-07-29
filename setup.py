12f93623e171ff8ebc71f94364b15f2b8effbb93
nikhil <nkmittal4994@gmail.com>
2015-07-29 17:37:05 +0530

    Setup files for pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name='indent',
+    packages=['indent'],  # this must be the same as the name above
+    version='0.1',
+    description='Indentation',
+    author='Nikhil Mittal',
+    author_email='nkmittal4994@gmail.com',
+    url='https://github.com/nickedes/indent',  # use the URL to the github repo
+    # I'll explain this in a second
+    download_url='https://github.com/nickedes/indent/tarball/0.1',
+    keywords=['testing', 'logging', 'example'],  # arbitrary keywords
+    classifiers=[],
+)
